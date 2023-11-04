1102d6396739bbb88db66e25b5b14c4cfffb524b
Muhammed Yousry <mohamedyousry250@gmail.com>
2023-11-04 23:09:29 +0200

    add room_types controller

---
+++ app/controllers/api/rooms/room_types_controller.rb
@@ -0,0 +1,44 @@
+
+module Api
+    module Rooms
+        class RoomTypesController < ApiController
+            def index
+                @room_types = RoomType.all
+                render json: RoomTypeSerializer.new(@room_types).serializable_hash.to_json
+            end
+          
+            def create
+                @room_type = RoomType.new(room_type_params)
+            
+                if @room_type.save
+                  render json: serialize_room_type, status: :created
+                else
+                  render json: { errors: @room_type.errors }, status: :unprocessable_entity
+                end
+              end
+          
+              def update
+                @room_type = RoomType.find(params[:id])
+            
+                if @room_type.update(room_type_params)
+                  render json: serialize_room_type, status: :ok
+                else
+                  render json: { errors: @room_type.errors }, status: :unprocessable_entity
+                end
+              end
+          
+            private
+          
+            def room_type_params
+                params.require(:data)
+                .require(:attributes)
+                .permit(:description, :capacity, :price_per_night)
+            end
+
+            def serialize_room_type
+              RoomTypeSerializer.new(@room_type).serializable_hash.to_json
+            end
+
+          end
+    end
+end
